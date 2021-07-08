b9ac005c42735365e5f37ddb702f842248cf3d40
ALbert-Shen-code <siyuanshen1993@gmail.com>
2021-07-08 19:16:11 +1000

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import react from "react"
+import {Container, AppBar, Typography, Grow, Grid} from "@material-ui/core";
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
